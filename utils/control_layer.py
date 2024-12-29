0622820a97b364680dd8462ccc889df97fcb2af9
Sayed Hossein Hosseini Dolat Abadi <s.hossein.hosseini1381@gmail.com>
2024-12-29 23:06:19 +0330

    Create a project and build a custom layer class to create custom packets

---
+++ utils/control_layer.py
@@ -0,0 +1,7 @@
+from scapy.all import Packet, IntField
+from scapy.fields import StrField
+
+
+class CustomLayer(Packet):
+    name = "ControlLayer"
+    fields_desc = [ IntField("more_chunk",0),IntField("seq_number",0), StrField("load", b"")]          # Field for the raw file data (string type)
