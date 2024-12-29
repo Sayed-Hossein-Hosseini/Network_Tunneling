a3e449527aa6ab9b437a1f3e585058697b2dfef8
Sayed Hossein Hosseini Dolat Abadi <s.hossein.hosseini1381@gmail.com>
2024-12-29 23:16:17 +0330

    Creating a text file to read

--- utils/utills.py
+++ utils/utills.py
@@ -12,3 +12,15 @@ class PacketHandler:
 
     def __init__(self, logger_service=None):
         self.logger_service = logger_service or LoggerService()
+
+
+    @staticmethod
+    def read_file(file_path):
+        try:
+            if not os.path.exists(file_path):
+                raise FileNotFoundError(f"File not found: {file_path}")
+            with open(file_path, "rb") as file:
+                file_data = file.read()
+            return file_data
+        except Exception as e:
+            raise Exception(f"Error reading file {file_path}: {e}")
