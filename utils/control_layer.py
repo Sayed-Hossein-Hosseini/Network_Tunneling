27d30cdd722ca6ab05018af25e6f622ee5d3c42f
Sayed Hossein Hosseini Dolat Abadi <s.hossein.hosseini1381@gmail.com>
2024-12-31 01:20:05 +0330

    utills

--- utils/control_layer.py
+++ utils/control_layer.py
@@ -5,4 +5,3 @@ from scapy.fields import StrField
 class CustomLayer(Packet):
     name = "ControlLayer"
     fields_desc = [ IntField("more_chunk",0),IntField("seq_number",0), StrField("load", b"")]          # Field for the raw file data (string type)
- 
